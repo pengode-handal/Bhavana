1c1f198cae7f4d10f876f52a0a69aa8a11972783
pengode-handal <saifpatih@gmail.com>
2024-04-10 22:55:20 +0700

    Hampir

--- public/js/index.js
+++ public/js/index.js
@@ -76,8 +76,6 @@ Particles.init({
         autoplay: true,
         dots: true,
         loop: true,
-        nav: true,
-        navText: ["Previous", "Next"],
         responsive: {
             0: { items: 4 },
             768: { items: 5 },
